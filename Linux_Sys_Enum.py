a453d5f3737b1c73e6d22451167d0c25a917854a
TC Johnson <superteece@gmail.com>
2015-11-06 20:01:36 -0700

    Added start of enumServices() function. Added Get Everything to menu to run all functions together.

--- Linux_Sys_Enum.py
+++ Linux_Sys_Enum.py
@@ -1,45 +1,38 @@
 import subprocess
 
-def mainMenu():
-    print("\n1. Gather User Info")
-    print("2. Gather Install Apps Info")
-    print("3. Gather Services Info")
-
-    menuSelection = int(input("\nEnter Selection: "))
-
-    return menuSelection
 
 def enumUsers():
     #Run id and output result to user_info.txt
-    with open("user_info/user_info.txt", "w") as f:
+    #subprocess.call(["mkdir", "user_info"])
+    with open("user_info.txt", "a") as f:
         f.write("###OUTPUT OF ID###\n")
         f.close()
-    with open("user_info/user_info.txt", "ab") as f:
+    with open("user_info.txt", "ab") as f:
         f.write(subprocess.check_output('id'))
         f.close()
-    with open("user_info/user_info.txt", "a") as f:
+    with open("user_info.txt", "a") as f:
         f.write("\n============BREAK===============\n\n")
         f.close()
 
     #Run who and append result to user_info.txt
-    with open("user_info/user_info.txt", "a") as f:
+    with open("user_info.txt", "a") as f:
         f.write("\n###OUTPUT OF WHO###\n")
         f.close()
-    with open("user_info/user_info.txt", "ab") as f:
+    with open("user_info.txt", "ab") as f:
         f.write(subprocess.check_output('who'))
         f.close()
-    with open("user_info/user_info.txt", "a") as f:
+    with open("user_info.txt", "a") as f:
         f.write("\n============BREAK===============\n\n")
         f.close()
 
     #Run w and append result to user_info.txt
-    with open("user_info/user_info.txt", "a") as f:
+    with open("user_info.txt", "a") as f:
         f.write("\n###OUTPUT OF W###\n")
         f.close()
-    with open("user_info/user_info.txt", "ab") as f:
+    with open("user_info.txt", "ab") as f:
         f.write(subprocess.check_output('w'))
         f.close()
-    with open("user_info/user_info.txt", "a") as f:
+    with open("user_info.txt", "a") as f:
         f.write("\n============BREAK===============\n\n")
         f.close()
 
@@ -51,13 +44,13 @@ def enumUsers():
 def enumApps():
 
     #Run w and append result to user_info.txt
-    with open("app_info/app_info.txt", "a") as f:
+    with open("app_info.txt", "a") as f:
         f.write("\n###Contents of /usr/bin###\n")
         f.close()
-    with open("app_info/app_info.txt", "ab") as f:
+    with open("app_info.txt", "ab") as f:
         f.write(subprocess.check_output(["ls", "-alh", "/usr/bin/"]))
         f.close()
-    with open("app_info/app_info.txt", "a") as f:
+    with open("app_info.txt", "a") as f:
         f.write("\n============BREAK===============\n\n")
         f.close()
 
@@ -65,21 +58,45 @@ def enumApps():
     main()
 
 def enumServices():
-    print("Place Holder")
-    main()
+    #Run and append result to user_info.txt
+    with open("services_info.txt", "a") as f:
+        f.write("\n###Result of ps aux###\n")
+        f.close()
+    with open("services_info.txt", "ab") as f:
+        f.write(subprocess.check_output(["ps", "aux"]))
+        f.close()
+    with open("services_info.txt", "a") as f:
+        f.write("\n============BREAK===============\n\n")
+        f.close()
 
 def main():
-    menuSelection = mainMenu()
 
-    if menuSelection < 1 or menuSelection > 3:
-        print("\nInvalid selection")
-        mainMenu()
-    elif menuSelection == 1:
-        enumUsers()
-    elif menuSelection == 2:
-        enumApps()
-    elif menuSelection == 3:
-        enumServices()
+    menu = {}
+    menu['1']="Gather User Info"
+    menu['2']="Gather Installed Apps Info"
+    menu['3']="Gather Services Info"
+    menu['4']='Get Everything'
+    menu['5']="Exit"
+    while True:
+        for entry in sorted(menu, key=int):
+            print(entry, menu[entry])
+
+        selection=input("\nPlease Select: ")
+        if selection =='1':
+            enumUsers()
+        elif selection == '2':
+            enumApps()
+        elif selection == '3':
+            enumServices()
+        elif selection == '4':
+            enumApps()
+            enumUsers()
+            enumServices()
+        elif selection == '5':
+            break
+        else:
+            print("Unknown Option Selected!")
+
 
 
 main()
